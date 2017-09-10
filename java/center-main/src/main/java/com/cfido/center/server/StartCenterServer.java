31dc502706ec8f6a890466a3c2322ff8d2afe2cf
梁韦江 <liangwj72@qq.com>
2017-09-10 14:31:47 +0800

    启动时，自动设置系统名字

--- java/center-main/src/main/java/com/cfido/center/server/StartCenterServer.java
+++ java/center-main/src/main/java/com/cfido/center/server/StartCenterServer.java
@@ -12,6 +12,7 @@ import com.cfido.center.server.config.MonitorServerProperties;
 import com.cfido.commons.spring.anno.EnableRedisCache;
 import com.cfido.commons.spring.anno.EnableSmsCodeService;
 import com.cfido.commons.spring.anno.EnableWebPorjectSet;
+import com.cfido.commons.spring.dict.core.DictCoreService;
 import com.cfido.commons.spring.monitor.MonitorClientProperties;
 
 import game.other.sms.gw253.EnableSmsGw253;
@@ -28,6 +29,9 @@ import game.other.sms.gw253.EnableSmsGw253;
 public class StartCenterServer {
 
 	@Autowired
+	private DictCoreService dictCoreService;
+
+	@Autowired
 	private MonitorClientProperties clientProp;
 
 	public static void main(String[] args) {
@@ -38,6 +42,9 @@ public class StartCenterServer {
 	protected void init() {
 		// 设置监控客户端无需汇报给自己
 		this.clientProp.setEnable(false);
+		
+		this.dictCoreService.getRawText("system.name", "中心服务器");
+		
 	}
 
 }
